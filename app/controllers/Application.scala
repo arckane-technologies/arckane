6de929e141830bfe489b90ec4b62af518f357dc9
Francisco Arámburo <atfm05@gmail.com>
2015-08-25 00:24:36 -0500

    #13 Finished.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,8 +12,16 @@ import decision.user._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index())
+  def index = Action { request =>
+    val userId = """/user/(.+)""".r
+    request.session.get("home").map { url =>
+      url match {
+        case userId(home) => Redirect(routes.Application.user(home))
+        case _ => InternalServerError("Invalid home value in session.")
+      }
+    }.getOrElse {
+      Ok(views.html.index())
+    }
   }
 
   def signin = Action.async { request =>
@@ -22,6 +30,10 @@ class Application extends Controller {
       val password = form("password").head
       UserTag.authenticate(email, password).map {
         case Some(user) => Ok(Json.obj("success" -> true, "user" -> user))
+          .withSession(
+            "connected" -> (user \ "email").as[String],
+            "name" -> (user \ "name").as[String],
+            "home" -> (user \ "url").as[String])
         case None => Ok(Json.obj("success" -> false))
       }
     }.getOrElse {
@@ -29,4 +41,16 @@ class Application extends Controller {
     }
   }
 
+  def signout = Action {
+    Ok(views.html.index()).withNewSession
+  }
+
+  def user (id: String) = Action { request =>
+    // CHANGE ME: Load from db the user profile
+    request.session.get("name").map { name =>
+      Ok(views.html.user("/user/"+id, name))
+    }.getOrElse {
+      InternalServerError("Invalid name value in session.")
+    }
+  }
 }
