c2278f2c709ef579a1e10ec1c8f31d8f4fe3fc56
Francisco Arámburo <atfm05@gmail.com>
2015-08-26 01:55:38 -0500

    #22 Basic page for skillbooks.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,8 @@ import play.api.libs.concurrent.Execution.Implicits._
 
 import database.persistence._
 import decision.user._
+import decision.skill._
+import decision.skillbook._
 
 class Application extends Controller {
 
@@ -51,4 +53,44 @@ class Application extends Controller {
       case None => NotFound("ERROR 404: User "+id+" not found.")
     }
   }
+
+  def skillbook (id: String) = Action.async { request =>
+    SkillbookTag.getPageData("/skillbook/"+id).map {
+      case Some(pageData) => Ok(views.html.book(pageData("name"), pageData("url"), pageData("description"), pageData("data")))
+      case None => NotFound("ERROR 404: Skillbook "+id+" not found.")
+    }
+  }
+
+  def skill (book: String, skill: String) = Action.async { request =>
+    Future(Ok("Hola :p"))
+  }
+  /*
+  """{"related": [
+    {"name": "Variable", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Some description..."},
+    {"name": "Evaluation", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "While Loops", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "},
+    {"name": "Functional Programming", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "In computer science, functional programming is a programming paradigm—a style of building the structure and elements of computer programs—that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data."},
+    {"name": "Evaluation", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "While Loops", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "Variable", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Some description..."},
+    {"name": "Evaluation", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "While Loops", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "Variable", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Some description..."},
+    {"name": "Evaluation", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "While Loops", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "Variable", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Some description..."},
+    {"name": "Evaluation", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."},
+    {"name": "While Loops", "infusionTarget": "/skill/123", "url": "/book/123/skill/123", "content": "Something..."}
+  ], "resources": [
+    {"name": "Some Book Chapter", "resourceType": "book", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "Cool Video", "resourceType": "video", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "May be Legal PDF", "resourceType": "pdf", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "n-person Games Equilibrium", "resourceType": "paper", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "Standford R01-1", "resourceType": "lecture", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "Google Edu", "resourceType": "slides", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "W3School: HTML", "resourceType": "article", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "Amazing Infographic", "resourceType": "image", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."},
+    {"name": "Scala at the Sea", "resourceType": "event", "infusionTarget": "/resource/0", "url": "/skill/123", "content": "Something..."}
+  ]}"""
+  */
 }
--- app/decision/Skillbook.scala
+++ app/decision/Skillbook.scala
@@ -6,26 +6,38 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.libs.concurrent.Execution.Implicits._
 
+import database.neo4j._
 import database.persistence._
 
 package object skillbook {
 
   trait Skillbook
   object SkillbookTag extends Tag[Skillbook]("Skillbook")
-  case class SkillbookInfo (name: String, description: String)
 
-  implicit val skillbookInfoWrites = new Writes[SkillbookInfo] {
-    def writes(props: SkillbookInfo) = Json.obj(
-      "name" -> props.name,
-      "description" -> props.description
-    )
+  implicit class SkillbookTagOps (tag: Tag[Skillbook]) {
+
+    def getPageData (url: String): Future[Option[Map[String, String]]] = for {
+      result <- query(Json.arr(Json.obj(
+        "statement" -> ("MATCH (n:"+tag.str+" {url: {urlmatcher}}) RETURN n.name, n.description"),
+        "parameters" -> Json.obj(
+          "urlmatcher" -> url
+        )), Json.obj(
+        "statement" -> ("MATCH (n:"+tag.str+" {url: {urlmatcher}})-[:ROOT]->(s:Skill) RETURN s.name, s.url, s.description"),
+        "parameters" -> Json.obj(
+          "urlmatcher" -> url
+        ))))
+    } yield if (result.length > 0)
+        Some(Map(
+          "url" -> url,
+          "name" -> result(0)("n.name")(0).as[String],
+          "description" -> result(0)("n.description")(0).as[String],
+          "data" -> Json.obj("related" -> Json.toJson(result(1)("s.name").zipWithIndex.map { case (name, index) => Json.obj(
+            "name" -> name,
+            "url" -> (url + result(1)("s.url")(index).as[String]),
+            "infusionTarget" -> result(1)("s.url")(index),
+            "content" -> result(1)("s.description")(index))
+        })).toString))
+      else
+        None
   }
-
-  implicit val skillbookInfoReads = (
-    (JsPath \ "name").read[String] and
-    (JsPath \ "description").read[String]
-  )(SkillbookInfo.apply _)
-
-  //def addRootSkill ()
-  //def removeRootSkill ()
 }
